c3d89ef5fe38d5a1f8ceeea92fb9b35d28e504e5
alik_put <56935749+alex2211-put@users.noreply.github.com>
2020-05-06 12:36:27 +0700

    Bags fixed

--- Subforwardlist.cpp
+++ Subforwardlist.cpp
@@ -4,31 +4,44 @@
 #include <iostream>
 #include "Subforwardlist.h"
 
-int Subforwardlist::pop_back() {
+int Subforwardlist::pop_back()
+{
 
-        Subforwardlist* p = nullptr;
-        Subforwardlist* a = next;
+    std::shared_ptr<Subforwardlist> p = nullptr;
+    std::shared_ptr<Subforwardlist> a = next;
+    if (a == nullptr)
+    {
+        return 0;
+    }
+    if (a->next != nullptr)
+    {
         while (a->next != nullptr)
         {
             p = a;
             a = a->next;
         }
+    }
+    if (p != nullptr)
         a = p;
-        if (a != nullptr)
-        {
-            int j = a->next->data;
-            a->next = nullptr;
-            std::cout << j;
-            return j;
-        } else std::cout << "Error";
-        return 0;
-    };
+    if (a->next != nullptr)
+    {
+        int j = a->next->data;
+        a->next = nullptr;
+        return j;
+    } else
+    {
+        int j = next->data;
+        next = nullptr;
+        return j;
+    }
+};
+
 bool Subforwardlist::push_back(int d)
 {
-    Subforwardlist* a = next;
+    std::shared_ptr<Subforwardlist> a = next;
     if (next == nullptr)
     {
-        next = new Subforwardlist;
+        next = std::shared_ptr<Subforwardlist>(new Subforwardlist);
         (next)->next = nullptr;
         (next)->data = d;
     } else
@@ -37,88 +50,135 @@ bool Subforwardlist::push_back(int d)
         {
             a = a->next;
         }
-        a->next = new Subforwardlist;
-        a = a->next;
-        a->data = d;
-        a->next = nullptr;
+        a->next = std::shared_ptr<Subforwardlist>(new Subforwardlist);
+        a->next->data = d;
+        a->next->next = nullptr;
     }
     return true;
 };
+
 bool Subforwardlist::show()
 {
-    Subforwardlist* a = next;
+    std::shared_ptr<Subforwardlist> a = next;
     while (a->next != nullptr)
     {
-        std::cout << a->data << " ";
         a = a->next;
     }
-    std::cout << a->data;
     return true;
 };
+
 bool Subforwardlist::push_forward(int d)
 {
-    Subforwardlist* b = next;
-    next = new Subforwardlist;
-    (next)->data = d;
-    (next)->next = b;
-    return true;
+    if (next != nullptr)
+    {
+        std::shared_ptr<Subforwardlist> b = next;
+        next = std::shared_ptr<Subforwardlist>(new Subforwardlist);
+        (next)->data = d;
+        (next)->next = b;
+        return true;
+    } else
+    {
+        next = std::shared_ptr<Subforwardlist>(new Subforwardlist);
+        (next)->data = d;
+        next->next = nullptr;
+        return true;
+    }
 };
+
 int Subforwardlist::pop_forward()
 {
-    Subforwardlist* b = next;
-    std::cout << b->data;
-    next = b->next;
-    return 0;
+    if (next != nullptr)
+    {
+        int s = next->data;
+        if (next->next != nullptr)
+        {
+            std::shared_ptr<Subforwardlist> b = next;
+            std::shared_ptr<Subforwardlist> a = next;
+            a = a->next;
+            next = a;
+        }
+        else {
+            next = nullptr;
+        }
+        return s;
+    } else return 0;
 };
+
 bool Subforwardlist::push_where(unsigned int where, int d)
 {
-    int s = 1;
-    Subforwardlist *a = next;
-    while (s != where)
+    if (where == 0)
     {
-        a = a->next;
-        s++;
+        std::shared_ptr<Subforwardlist> a = next;
+        next = std::shared_ptr<Subforwardlist>(new Subforwardlist);
+        (next)->data = d;
+        (next)->next = a;
+        return true;
+    } else
+    {
+        int s = 0;
+        std::shared_ptr<Subforwardlist> a = next;
+        std::shared_ptr<Subforwardlist> b = a;
+        while (s != where && a->next != nullptr)
+        {
+            b = a;
+            a = a->next;
+            s++;
+        }
+        std::shared_ptr<Subforwardlist> c = a;
+        a = std::shared_ptr<Subforwardlist>(new Subforwardlist);
+        a->data = d;
+        a->next = c;
+        b->next = a;
+        return true;
     }
-    Subforwardlist *b = a->next;
-    a->next = new Subforwardlist;
-    a->data = d;
-    a->next = b;
-    return true;
 };
+
 bool Subforwardlist::erase_where(unsigned where)
 {
-    Subforwardlist *a = next;
-    Subforwardlist *b;
-    int s = 1;
-    while (s != where - 1)
+    if (where == 0)
     {
-        a = a->next;
-        s++;
+        if (next->next != nullptr)
+        {
+            {
+                next = next->next;
+            }
+        } else next = nullptr;
+        return true;
+    } else
+    {
+        std::shared_ptr<Subforwardlist>  a = next;
+        std::shared_ptr<Subforwardlist>  b = nullptr;
+        int s = 0;
+        while (s != where - 1)
+        {
+            b = a;
+            a = a->next;
+            s++;
+        }
+        if (a->next == nullptr)
+        {
+            b->next = nullptr;
+            return true;
+        }
+        b = a->next->next;
+        a->next = b;
+        return true;
     }
-    b = a->next->next;
-    a->next = nullptr;
-    a->next = b;
-    return true;
 };
-unsigned int Subforwardlist::size()
+
+unsigned int Subforwardlist::get_size()
 {
-    Subforwardlist *a = next;
+    std::shared_ptr<Subforwardlist>  a = next;
     unsigned int s = 0;
     while (a != nullptr)
     {
-        a = a->next;
         s++;
+        a = a->next;
     }
-    std::cout << s;
     return s;
 };
+
 void Subforwardlist::clear()
 {
-    Subforwardlist *a = next;
-    while (a->next != nullptr)
-    {
-        a->data = 0;
-        a->next = nullptr;
-
-    }
+    next = nullptr;
 };
--- Subforwardlist.h
+++ Subforwardlist.h
@@ -4,26 +4,49 @@
 
 #ifndef STACK_SUBFORWARDLIST_H
 #define STACK_SUBFORWARDLIST_H
+
+#include <memory>
 #include "Stack.h"
 
-class Subforwardlist: private Stack {
+class Subforwardlist : private Stack
+{
 private:
     int data;
-    Subforwardlist* next;
+    std::shared_ptr<Subforwardlist> next;
 public:
     Subforwardlist()
     {
         data = 0;
         next = nullptr;
     }
-    int pop_back();
-    bool push_back(int d);
-    bool show();
+
+    ~Subforwardlist()
+    {
+        std::shared_ptr<Subforwardlist> a = next;
+        if (next != nullptr)
+            while (next->next != nullptr)
+            {
+                next = next->next;
+                a = next;
+            }
+    }
+
+    int pop_back() override;
+
+    bool push_back(int d) override;
+
+    bool show() override;
+
     bool push_forward(int d);
+
     int pop_forward();
+
     bool push_where(unsigned int where, int d);
+
     bool erase_where(unsigned where);
-    unsigned int size();
+
+    unsigned int get_size();
+
     void clear();
 };
 
